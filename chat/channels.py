d73ec7c41f1454f377156c27ef036d22bd159522
Prodip007 <p.k.sorkar007@gmail.com>
2022-12-24 23:48:51 +0600

    BroadCastWebSocketChannel

--- chat/channels.py
+++ chat/channels.py
@@ -4,3 +4,17 @@ import pika
 
 from notifications.channels import BaseNotificationChannel
 
+
+class BroadCastWebSocketChannel(BaseNotificationChannel):
+    """Fanout notification channel with RabbitMQ."""
+
+    def _connect(self):
+        """Connect to the RabbitMQ server."""
+
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost')
+        )
+
+        channel = connection.channel()
+
+        return connection, channel
